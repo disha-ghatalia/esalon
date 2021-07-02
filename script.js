67d3d88baa96bc617bcaa75a69055de84bc7bef9
Disha Ghatalia <d.ghatalia@northeastern.edu>
2021-07-02 13:15:15 -0400

    fixing some errs

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
+document.body.classList.remove('hidden');
+document.body.classList.add('visible');
+
 var d = new Date();
 
-let currentYear = new Date().getFullYear();
+var currentYear = new Date().getFullYear();
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
+var monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
-for(m = 0; m <= 11; m++) {
-    var optn = document.createElement("OPTION");
-    optn.text = monthNames[m];
-    optn.value = (m+1);
-   
-    document.getElementById('month').options.add(optn);
-}
 
-for(var i = 1; i <= 31; i++) {
-    var optn = document.createElement("OPTION");
-    optn.text = i;
-    optn.value = i;
-   
-    document.getElementById('date').options.add(optn);
-}
+var form = document.getElementById("edit-pet"); 
 
-for(var j = 1950; j <= currentYear; j++) {
-    var optn = document.createElement("OPTION");
-    optn.text = j;
-    optn.value = j;
-   
-    document.getElementById('year').options.add(optn);
-}
+if(form){
+
+    for(m = 0; m <= 11; m++) {
+        var optn = document.createElement("OPTION");
+        optn.text = monthNames[m];
+        optn.value = (m+1);
+    
+        document.getElementById('month').options.add(optn);
+    }
+
+    for(var i = 1; i <= 31; i++) {
+        var optn = document.createElement("OPTION");
+        optn.text = i;
+        optn.value = i;
+    
+        document.getElementById('date').options.add(optn);
+    }
+
+    for(var j = 1950; j <= currentYear; j++) {
+        var optn = document.createElement("OPTION");
+        optn.text = j;
+        optn.value = j;
+    
+        document.getElementById('year').options.add(optn);
+    }
+  }
